f2d7bc286d7c5358d51174a007638aa614cb633d
Leo <64216877+wangrunlin@users.noreply.github.com>
2023-08-25 04:03:19 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Leo Wang\'s Notion Blog',
   author: 'Leo Wang',
   email: 'alin.runlin@gmail.com',
-  link: 'https://nobelium.vercel.app',
+  link: 'https://blog.wangrunlin.com',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
